69a349a89cea97a220b03a87bb605599d2b2ba78
Benjamin Thiele <otherwisede@gmail.com>
2022-04-22 08:48:15 +0200

    Required Moment to convert to better timezone, suppressed deprecation warnings

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ console.log(`[TWITTER] Starting Service`);
 
 // Packages
 const {Discord, Client, Collection, Message, MessageEmbed} = require("discord.js");
+//require node-tweet-stream
 const Twit = require("node-tweet-stream");
+//require fs
 const fs = require("fs");
+//require Moment & suppress Deprecation Warnings
+const moment = require('moment-timezone');
+moment.suppressDeprecationWarnings = true;
 
 // Setup
 const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
@@ -30,7 +35,7 @@ dClient.on('ready', () => {
 // Tweet Listener + Post
 t.on('tweet', function (tweet) {
     let media = tweet.entities.media;
-    chatPost(tweet.text, tweet.user.screen_name, `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`, tweet.created_at, tweet.user.profile_image_url, media);
+    chatPost(tweet.text, tweet.user.screen_name, `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`, moment.utc(tweet.created_at).tz('Europe/Berlin').format('DD-MM-YYYY HH:mm:ss'), tweet.user.profile_image_url, media);
 })
 t.on('error', function (err) {
     console.log('Oh no')
